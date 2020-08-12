948c1f48aefc2d43d0331928a8b177815c101024
Beatriz Ferreira dos Santos <bfsantos@RDL724796.raiadrogasil.com>
2020-08-12 17:53:49 -0300

    estrutura inicial Sign In

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,48 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Platform ,KeyboardAvoidingView, Text, TextInput, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
-const SignIn = () => <View style={styles.container} />;
+const SignIn = () => (
+    <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding': null} style={styles.container}>
+        <View >
+            <Text style={styles.title}>
+                Entrar
+            </Text>
+
+            <Text style={styles.label}>
+                E-MAIL
+            </Text>   
+            <TextInput
+              style={styles.input}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              underlineColorAndroid="transparent"
+              autoFocus
+              returnKeyType="next"
+            /> 
+
+            <Text style={styles.label}>
+                SENHA
+            </Text>   
+
+            <TextInput
+              style={styles.input}
+              secureTextEntry
+              autoCorrect={false}
+              autoCapitalize="none"
+              underlineColorAndroid="transparent"
+              returnKeyType="send"
+            />
+
+            <TouchableOpacity onPress={()=>{}} style={styles.button}>
+                <Text style={styles.buttonText}>
+                  Entrar
+                </Text>
+            </TouchableOpacity>
+        </View>
+   </KeyboardAvoidingView>
+);
 
 
 export default SignIn;
