aeec221770d3dc35592f42cc8e04e4ce9c1a56a6
Beatriz Ferreira dos Santos <bfsantos@RDL724796.raiadrogasil.com>
2020-08-12 17:11:17 -0300

    estrutura inicial

---
+++ src/pages/SignIn/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+import { colors, general } from '../../styles';
+
+const styles = StyleSheet.create({
+  ...general.formStyles,
+  ...general.buttonStyles,
+
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    padding: 30,
+  },
+ teste:{
+   fontSize: 28
+ },
+  title: {
+    fontSize: 28,
+    marginBottom: 20,
+    color: colors.white,
+    textAlign: 'center',
+  },
+});
+
+export default styles;
